a8dad5b724a43c95da847b1c965f7e5fab9bbbab
Niyojeet Shrirao <niyojeetshrirao@gmail.com>
2025-05-10 20:25:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World from k3s!- a demo app"
+    return "Hello, World from k3s!- a demo app for testing github actions"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
